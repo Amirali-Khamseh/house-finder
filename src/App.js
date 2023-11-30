d07eb7d15112dbf3d5a07727af424b78de8e226f
Amir ali <amiralikhamseh01@gmail.com>
2023-11-30 21:42:37 +0100

    React router configured for different routes and different pages of the app has been built

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import "./App.css";
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import ForgotPassowrd from "./pages/ForgotPassowrd";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import Profile from "./pages/Profile";
+import Offers from "./pages/Offers";
+import Page404 from "./pages/Page404";
 function App() {
   return (
-    <div className="App">
-      <p className="text-2xl "> hi</p>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/forgot-password" element={<ForgotPassowrd />}></Route>
+          <Route path="/sign-in" element={<Signin />}></Route>
+          <Route path="/sign-up" element={<Signup />}></Route>
+          <Route path="/profile" element={<Profile />}></Route>
+          <Route path="/offers" element={<Offers />}></Route>
+          <Route path="/*" element={<Page404 />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
