7a6629228ebbd5aad63534d6889ad26fa4bb0d8f
Amir ali <amiralikhamseh01@gmail.com>
2023-12-05 13:31:46 +0100

    Google auth added for sign-in and sign-up

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -1,7 +1,41 @@
 import { FcGoogle } from "react-icons/fc";
+import { toast } from "react-toastify";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { doc, getDoc, serverTimestamp, setDoc } from "firebase/firestore";
+import { db } from "../firebase";
+import { useNavigate } from "react-router-dom";
+
 function OAuth() {
+  const navigate = useNavigate();
+  async function onGoogleClick() {
+    try {
+      const auth = getAuth();
+      const provider = new GoogleAuthProvider();
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
+
+      // check for the user
+
+      const docRef = doc(db, "users", user.uid);
+      const docSnap = await getDoc(docRef);
+      if (!docSnap.exists()) {
+        await setDoc(docRef, {
+          name: user.displayName,
+          email: user.email,
+          timestamp: serverTimestamp(),
+        });
+      }
+      navigate("/");
+    } catch (error) {
+      toast.error("Could not authorize with Google");
+    }
+  }
   return (
-    <button className=" bg-black  px-7 py-3  rounded-xl flex items-center justify-center w-full text-white  hover:bg-gray-700 hover:scale-90 hover:transition-all ease-in-out duration-150">
+    <button
+      onClick={onGoogleClick}
+      className=" bg-black  px-7 py-3  rounded-xl flex items-center justify-center w-full text-white  hover:bg-gray-700 hover:scale-90 hover:transition-all ease-in-out duration-150"
+      type="button"
+    >
       <FcGoogle className="text-xl mr-2 bg-white rounded" />
       Continue with Google
     </button>
