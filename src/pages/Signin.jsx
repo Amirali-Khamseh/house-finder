903f09d62804250ae505c50dd1a65e594591d425
Amir ali <amiralikhamseh01@gmail.com>
2023-12-16 10:10:16 +0100

    background and google icon fixed

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -114,10 +114,10 @@ export default function Signin() {
                 </Link>
               </p>
             </div>
-            <div>
+            <div className="w-full">
               <button
                 type="submit"
-                className="w-full mt-2 text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+                className="w-[195px] ml-[7.5rem] mt-2 text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
               >
                 Sign-in
               </button>
